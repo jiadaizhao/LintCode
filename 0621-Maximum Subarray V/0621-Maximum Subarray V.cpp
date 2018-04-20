defaacb47809f8346f5f24d617077a08197bbd77
jiadaizhao <jiadaizhao@gmail.com>
2018-04-20 10:29:29 -0700

    Update problem 621

--- 0621-Maximum Subarray V/0621-Maximum Subarray V.cpp
+++ 0621-Maximum Subarray V/0621-Maximum Subarray V.cpp
@@ -2,27 +2,35 @@ class Solution {
 public:
     /**
      * @param nums an array of integers
-     * @param k an integer
+     * @param k1 an integer
+     * @param k2 an integer
      * @return the largest sum
      */
-    int maxSubarray4(vector<int>& nums, int k) {
+    int maxSubarray5(vector<int>& nums, int k1, int k2) {
         // Write your code here
-        if (nums.size() < k) {
+        if (nums.size() < k1) {
             return 0;
         }
         
-        int sum = 0;
-        for (int i = 0; i < k; ++i) {
-            sum += nums[i];
-        }
-        int maxSum = sum;
-        int presum = 0;
-        int premin = 0;
-        for (int i = k; i < nums.size(); ++i) {
-            sum += nums[i];
-            presum += nums[i - k];
-            premin = min(premin, presum);
-            maxSum = max(maxSum, sum - premin);
+        deque<int> dq;
+        vector<int> sum(1 + nums.size());
+        int maxSum = INT_MIN;
+        for (int i = 1; i <= nums.size(); ++i) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+            while (!dq.empty() && dq.front() < i - k2) {
+                dq.pop_front();
+            }
+            
+            if (i >= k1) {
+                while (!dq.empty() && sum[dq.back()] > sum[i - k1]) {
+                    dq.pop_back();
+                }
+                dq.push_back(i - k1);
+            }
+            
+            if (!dq.empty()) {
+                maxSum = max(maxSum, sum[i] - sum[dq.front()]);
+            }
         }
         
         return maxSum;
--- 0621-Maximum Subarray V/README.md
+++ 0621-Maximum Subarray V/README.md
@@ -4,12 +4,12 @@ Difficulty: Hard
 
 http://www.lintcode.com/en/problem/maximum-subarray-v/
 
-Given an integer arrays, find a contiguous subarray which has the largest sum and length should be greater or equal to given length k.
-Return the largest sum, return 0 if there are fewer than k elements in the array.
+Given an integer arrays, find a contiguous subarray which has the largest sum and length should be between k1 and k2 (include k1 and k2).
+Return the largest sum, return 0 if there are fewer than k1 elements in the array.
 
 **Notice**  
 
 Ensure that the result is an integer type.
 
 **Example**  
-Given the array [-2,2,-3,4,-1,2,1,-5,3] and k = 5, the contiguous subarray [2,-3,4,-1,2,1] has the largest sum = 5.
+Given the array [-2,2,-3,4,-1,2,1,-5,3] and k1 = 2, k2 = 4, the contiguous subarray [4,-1,2,1] has the largest sum = 6.
